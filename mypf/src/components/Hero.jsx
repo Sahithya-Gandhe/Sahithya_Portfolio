be6b64be4c326b5bd9be75cea836e75f1829dc8a
Sahithya-Gandhe <sahithyagandhe@gmail.com>
2025-08-01 21:36:05 +0530

    Update Hero.jsx
    .

--- mypf/src/components/Hero.jsx
+++ mypf/src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           />
         </div>
         <div className="hero-text">
-          <h1>Hello, I'm Sahithya Gande</h1>
+          <h1>Hello, I'm Sahithya</h1>
           <p>A passionate AI Enthusiast and Full Stack Developer with a keen interest in building innovative solutions. Currently pursuing B.Tech in Computer Science Engineering, I combine technical expertise with creative problem-solving to deliver impactful results.</p>
           <p>As an undergrad studying in Computer Science and Engineering, I have a strong passion for programming and web development. Skilled in Java , Python ,HTML, CSS, JavaScript,Reactjs ,Expressjs and backend technologies, Currently Exploring Artificial Intelligence and Machine Learning.
           I love building user-friendly and efficient applications , I'm looking for internship opportunities to apply my skills , gain hands-on experience, and contribute to real-world projects. Eager to learn, grow, and collaborate with like-minded professionals!</p>
